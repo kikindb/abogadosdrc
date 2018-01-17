27e3912007f84493a886ab0507e154ca7bd5cfd3
Jesus Enrique Dick Bustamante <enriquedb2@gmail.com>
2018-01-17 17:21:15 -0600

    Home casi terminado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,17 @@ import { APP_ROUTING } from './app.routes';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ContactoComponent } from './components/contacto/contacto.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    NavbarComponent
+    NavbarComponent,
+    FooterComponent,
+    ContactoComponent
   ],
   imports: [
     BrowserModule,
